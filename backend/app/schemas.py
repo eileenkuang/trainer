fcd48b08984401d9203b722bc134996cd1986ed3
issixe <isabel.liu747@gmail.com>
2026-01-17 14:08:44 -0500

    frontend

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel
 
+#Request schemas
+class ExerciseRequest(BaseModel):
+    exercise: str
+
+class UploadRequest(BaseModel):
+    video_path: str
+
 class AnalyzeRequest(BaseModel):
     text: str
 
+#Response schemas
 class AnalyzeResponse(BaseModel):
-    confidence: float
-    note: str
+    video_path: str
+
+class SummaryResponse(BaseModel):
+    summary: str
